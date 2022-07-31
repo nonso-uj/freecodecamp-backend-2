8256eeb42ca97dccd40bf909cc22e8131f85ff35
nonso-uj <nonso.udonne@gmail.com>
2022-07-31 23:54:53 +0100

    done

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+// language: req.headers["accept-language"],
+// start
+app.get('/api/whoami', function(req, res){
+  res.json({
+    ip: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers['user-agent']
+  })
+})
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
